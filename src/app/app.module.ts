37e44bf695c4e1b7a9a6ac4b473ffd18fb390528
Henoch <henochcruz@gmail.com>
2020-09-23 11:02:26 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { FormlyBootstrapModule } from "@ngx-formly/bootstrap";
+import { FormlyFieldConfig, FormlyModule } from "@ngx-formly/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+export function ValidatorMessage(err, field: FormlyFieldConfig) {
+  return `"${field.templateOptions.label}" es un campo requerido`;
+}
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule,ReactiveFormsModule,
+    FormsModule,
+    NgxDyncmpModule,
+     FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: ValidatorMessage  },
+      ],
+    }),
+    FormlyBootstrapModule,
+    NgbModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
